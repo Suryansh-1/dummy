0590d9bfb1584ede769b8e9d8174a3401c470b42
Suryansh-1 <suryanshgupta404@gmail.com>
2024-06-30 17:02:03 +0530

    first commit

---
+++ add.js
@@ -0,0 +1,7 @@
+const num1=10
+const num2=12
+
+function addValue(num1,num2){
+    console.log(`The  sum is :${num1+num2}`)
+}
+addValue()
---
+++ variable.js
@@ -0,0 +1,7 @@
+//local
+const n1="Peter"
+//share
+const n2='jhon'
+const n3="Ram"
+
+module.exports={n2,n3}
